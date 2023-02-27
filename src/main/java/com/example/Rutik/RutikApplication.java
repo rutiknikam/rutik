d86a192d4b09597f4f54283f5518f8a0fe4f6068
rutiknikam <rutiknikam044@gmail.com>
2023-02-28 00:16:40 +0530

    first application

---
+++ src/main/java/com/example/Rutik/RutikApplication.java
@@ -0,0 +1,13 @@
+package com.example.Rutik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutikApplication.class, args);
+	}
+
+}
