a0fa67c9c6b5ac87bd2d8005e4a9ef1af32bfe7a
rutiknikam <rutiknikam044@gmail.com>
2023-03-05 08:40:30 +0530

    added

---
+++ src/main/java/com/example/Rutik/boot.java
@@ -0,0 +1,15 @@
+package com.example.Rutik;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class boot {
+    @GetMapping("/myboot")
+    public String getData(){
+        return "plz check error";
+    }
+
+
+
+}
